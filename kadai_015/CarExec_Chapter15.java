54be577f2d6cf2107f5785e5a5f9dbc1723935cc
ntm3878 <0re3878swmp@gmail.com>
2024-11-20 23:37:14 +0900

    kadai_15提出

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,15 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        //車クラスインスタンス
+		Car_chapter15 car_chapter15 = new Car_chapter15();
+		
+		//車クラスのgearChangeメソッド実行
+		car_chapter15.gearChange(3);
+	    //runメソッド実行
+        car_chapter15.run();
+ }
+}
